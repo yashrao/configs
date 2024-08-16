bb8e1d587dffa6d6b1ffa00700a957b126e7e435
Yash Rao <12raoy1@gmail.com>
2024-08-16 12:53:46 +0800

    updating emacs config

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -138,9 +138,18 @@
    ("pyls.plugins.pyls_isort.enabled" t t)))
 :hook
 ((python-mode . lsp)
- (solidity-mode . lsp)
+ ;;(solidity-mode . lsp)
  (tsx-ts-mode . lsp))
+ (rust-mode . lsp))
 )
+
+;; (use-package lsp-mode
+;;   :hook (solidity-mode . lsp-deferred)
+;;   :commands (lsp lsp-deferred)
+;;   :config
+;;   (setq lsp-solidity-server-command '("solc" "--lsp"))
+;;   (add-to-list 'lsp-language-id-configuration '(solidity-mode . "solidity")))
+
 (use-package lsp-ui :commands lsp-ui-mode)
 ;; Cheats for C/C++
 (use-package ccls      ;; Make sure ccls is installed on the system
@@ -197,7 +206,8 @@
 (set-frame-font "JetBrains Mono-12" nil t)
 ;;(load-theme 'modus-vivendi t)
 ;;(load-theme 'ef-bio t)
-(load-theme 'almost-mono-black t)
+;;(load-theme 'almost-mono-gray t)
+(load-theme 'doom-one t)
 ;; Spaces > tabs
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 4)
@@ -221,6 +231,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(c-basic-offset 4)
+ '(custom-safe-themes
+   '("dccf4a8f1aaf5f24d2ab63af1aa75fd9d535c83377f8e26380162e888be0c6a9" "c8c4baac2988652a760554e0e7ce11a0fe0f8468736be2b79355c9d9cc14b751" "13096a9a6e75c7330c1bc500f30a8f4407bd618431c94aeab55c9855731a95e1" "93011fe35859772a6766df8a4be817add8bfe105246173206478a0706f88b33d" "4b6cc3b60871e2f4f9a026a5c86df27905fb1b0e96277ff18a76a39ca53b82e1" "f4d1b183465f2d29b7a2e9dbe87ccc20598e79738e5d29fc52ec8fb8c576fcfd" "014cb63097fc7dbda3edf53eb09802237961cbb4c9e9abd705f23b86511b0a69" "5f128efd37c6a87cd4ad8e8b7f2afaba425425524a68133ac0efd87291d05874" "8f5b54bf6a36fe1c138219960dd324aad8ab1f62f543bed73ef5ad60956e36ae" "d0fd069415ef23ccc21ccb0e54d93bdbb996a6cce48ffce7f810826bb243502c" "ffba0482d3548c9494e84c1324d527f73ea4e43fff8dfd0e48faa8fc6d5c2bc7" "e410458d3e769c33e0865971deb6e8422457fad02bf51f7862fa180ccc42c032" "81f53ee9ddd3f8559f94c127c9327d578e264c574cda7c6d9daddaec226f87bb" "dfb1c8b5bfa040b042b4ef660d0aab48ef2e89ee719a1f24a4629a0c5ed769e8" "2b20b4633721cc23869499012a69894293d49e147feeb833663fdc968f240873" "f5f80dd6588e59cfc3ce2f11568ff8296717a938edd448a947f9823a4e282b66" "88f7ee5594021c60a4a6a1c275614103de8c1435d6d08cc58882f920e0cec65e" "6e18353d35efc18952c57d3c7ef966cad563dc65a2bba0660b951d990e23fc07" "d4b608d76e3a087b124c74c2b642c933d8121b24e53d4bbd5e7327c36cc69ccc" "6e33d3dd48bc8ed38fd501e84067d3c74dfabbfc6d345a92e24f39473096da3f" "8b148cf8154d34917dfc794b5d0fe65f21e9155977a36a5985f89c09a9669aa0" "d481904809c509641a1a1f1b1eb80b94c58c210145effc2631c1a7f2e4a2fdf4" "571661a9d205cb32dfed5566019ad54f5bb3415d2d88f7ea1d00c7c794e70a36" "34cf3305b35e3a8132a0b1bdf2c67623bc2cb05b125f8d7d26bd51fd16d547ec" "48042425e84cd92184837e01d0b4fe9f912d875c43021c3bcb7eeb51f1be5710" default))
  '(package-selected-packages '(hl-todo go-mode ccls evil)))
       
       ;;; backup/autosave - Changing the location of the autosaving
