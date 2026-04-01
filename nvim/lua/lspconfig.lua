dae8952f25340bf44b25a9a944943e4deee2b44c
Yash Rao <12raoy1@gmail.com>
2026-04-01 23:02:20 +0800

    added more lsp stuff

--- nvim/lua/lspconfig.lua
+++ nvim/lua/lspconfig.lua
@@ -30,14 +30,37 @@ vim.lsp.config['clangd'] = {
     settings = {}
 }
 
--- vim.lsp.config['clangd'] = {
---     cmd = { 'clangd' }, 
---     filetypes = { 'c', 'cpp' },
---     root_markets = { '.git', 'Makefile' },
---     settings = {}
--- }
+vim.lsp.config['solidity'] = {
+    cmd = { 'nomicfoundation-solidity-language-server', '--stdio' }, 
+    filetypes = { 'solidity' },
+    root_markets = { '.git', 'foundry.toml' },
+    settings = {}
+}
+
+vim.lsp.config['go'] = {
+    cmd = { 'gopls' },
+    filetypes = { 'go' },
+    root_markets = { '.git', 'go.mod' },
+    settings = {}
+}
+
+vim.lsp.config['typescript'] = {
+    cmd = { 'typescript-language-server', '--stdio' },
+    filetypes = { 'typescript', 'typescriptreact', 'javascript', 'javascriptreact' },
+    root_markets = { '.git', 'package.json' },
+    settings = {}
+}
+
+vim.lsp.config['zig'] = {
+    cmd = { 'zls' },
+    filetypes = { 'zig' },
+    root_markets = { '.git', 'build.zig' },
+    settings = {}
+}
 
 -- enabling all the lsps
 vim.lsp.enable('rust_analyzer')
 vim.lsp.enable('clangd')
-
+vim.lsp.enable('solidity')
+vim.lsp.enable('go')
+vim.lsp.enable('typescript')
